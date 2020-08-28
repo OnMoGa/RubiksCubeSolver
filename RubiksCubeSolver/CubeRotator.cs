81ba39a0de8ac8edf300640fa32937875a64a38d
Michael Hamilton <micky.hamilton@outlook.com>
2020-08-29 00:06:48 +1000

    All rotations working

--- RubiksCubeSolver/CubeRotator.cs
+++ RubiksCubeSolver/CubeRotator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace RubiksCubeSolver {
@@ -25,125 +26,209 @@ namespace RubiksCubeSolver {
 
 
 		public static Cube applyRotation(this Cube startingCube, Rotation rotation) {
-			switch (rotation) {
-				case Rotation.F: return startingCube.FRotation();
-				case Rotation.Fr: return startingCube.FrRotation();
-				case Rotation.R: return startingCube.RRotation();
-				case Rotation.Rr: return startingCube.RrRotation();
+			Cube newCube = startingCube.clone();
+
+			RotationSettings rotationSettings;
+
+			switch(rotation) {
+				case Rotation.F: rotationSettings = new FRotationSettings();
+					break;
+				case Rotation.Fr: rotationSettings = new FrRotationSettings();
+					break;
+				case Rotation.R:rotationSettings = new RRotationSettings();
+					break;
+				case Rotation.Rr: rotationSettings = new RrRotationSettings();
+					break;
+				case Rotation.U:rotationSettings = new URotationSettings();
+					break;
+				case Rotation.Ur: rotationSettings = new UrRotationSettings();
+					break;
+				case Rotation.L:rotationSettings = new LRotationSettings();
+					break;
+				case Rotation.Lr: rotationSettings = new LrRotationSettings();
+					break;
+				case Rotation.B:rotationSettings = new BRotationSettings();
+					break;
+				case Rotation.Br: rotationSettings = new BrRotationSettings();
+					break;
+				case Rotation.D:rotationSettings = new DRotationSettings();
+					break;
+				case Rotation.Dr: rotationSettings = new DrRotationSettings();
+					break;
+				default: return newCube;
 			}
-
-			return startingCube;
+			
+			newCube.appliedRotations.Add(rotation);
+			return newCube.Rotate(rotationSettings);
 		}
 
 
-		public static Cube FRotation(this Cube startingCube) {
-			Cube newCube = startingCube.clone();
+		private static Cube Rotate(this Cube newCube, RotationSettings rotationSettings) {
+			PropertyInfo horizontalCoordinate = rotationSettings.horizontalCoordinate;
+			PropertyInfo verticalCoordinate = rotationSettings.verticalCoordinate;
+			Dictionary<Direction, Direction> nextDirection = rotationSettings.nextDirection;
+			bool reversed = rotationSettings.reversed;
 
-
-
-			newCube.appliedRotations.Add(Rotation.F);
-			return newCube;
-		}
-
-
-		public static Cube FrRotation(this Cube startingCube) {
-			Cube newCube = startingCube.clone();
-
-
-
-			newCube.appliedRotations.Add(Rotation.Fr);
-			return newCube;
-		}
-
-
-		public static Cube RRotation(this Cube startingCube) {
-			Cube newCube = startingCube.clone();
-
-			List<Piece> rotatingFace = newCube.getCubeFace(Direction.Right).ToList();
+			List<Piece> rotatingFace = newCube.getCubeFace(rotationSettings.nextDirection.First(kvp => kvp.Key == kvp.Value).Value).ToList();
 
 			foreach (Piece p in rotatingFace) {
-				if (p.z == 0) {
-					p.z = p.y;
-					p.y = 2;
-				} else if (p.z == 1) {
-					int oldY = p.y;
-					p.y = p.z;
-					p.z = oldY;
-				} else if (p.z == 2) {
-					p.z = p.y;
-					p.y = 0;
+				int horizontalCoordinateValue = (int)horizontalCoordinate.GetValue(p);
+				int verticalCoordinateValue = (int)verticalCoordinate.GetValue(p);
+
+				if (horizontalCoordinateValue == 0) {
+					horizontalCoordinateValue = reversed ? 2-verticalCoordinateValue:verticalCoordinateValue;
+					verticalCoordinateValue = reversed ? 0 : 2;
+				} else if (horizontalCoordinateValue == 1) {
+					horizontalCoordinateValue = reversed ? 2-verticalCoordinateValue:verticalCoordinateValue;
+					verticalCoordinateValue = 1;
+				} else if (horizontalCoordinateValue == 2) {
+					horizontalCoordinateValue = reversed ? 2-verticalCoordinateValue:verticalCoordinateValue;
+					verticalCoordinateValue = reversed ? 2 : 0;
 				}
+				horizontalCoordinate.SetValue(p, horizontalCoordinateValue);
+				verticalCoordinate.SetValue(p, verticalCoordinateValue);
 
 				List<PieceFace> newFaces = new List<PieceFace>();
 				foreach (PieceFace pieceFace in p.faces) {
 					PieceFace newFace = pieceFace;
-					if (pieceFace.direction == Direction.Forwards) {
-						newFace.direction = Direction.Up;
-					} else if (pieceFace.direction == Direction.Up) {
-						newFace.direction = Direction.Backwards;
-					} else if (pieceFace.direction == Direction.Backwards) {
-						newFace.direction = Direction.Down;
-					} else if (pieceFace.direction == Direction.Down) {
-						newFace.direction = Direction.Forwards;
-					}
+					newFace.direction = nextDirection[pieceFace.direction];
 					newFaces.Add(newFace);
 					p.faces = newFaces;
 				}
 				
 			}
 
-			newCube.appliedRotations.Add(Rotation.R);
 			return newCube;
 		}
-
-
-
-		public static Cube RrRotation(this Cube startingCube) {
-			Cube newCube = startingCube.clone();
-
-			List<Piece> rotatingFace = newCube.getCubeFace(Direction.Right).ToList();
-
-			foreach (Piece p in rotatingFace) {
-				if (p.z == 0) {
-					p.z = 2-p.y;
-					p.y = 0;
-				} else if (p.z == 1) {
-					int oldY = p.y;
-					p.y = p.z;
-					p.z = 2 - oldY;
-				} else if (p.z == 2) {
-					p.z = 2 - p.y;
-					p.y = 2;
-				}
-
-				List<PieceFace> newFaces = new List<PieceFace>();
-				foreach (PieceFace pieceFace in p.faces) {
-					PieceFace newFace = pieceFace;
-					if (pieceFace.direction == Direction.Forwards) {
-						newFace.direction = Direction.Down;
-					} else if (pieceFace.direction == Direction.Down) {
-						newFace.direction = Direction.Backwards;
-					} else if (pieceFace.direction == Direction.Backwards) {
-						newFace.direction = Direction.Up;
-					} else if (pieceFace.direction == Direction.Up) {
-						newFace.direction = Direction.Forwards;
-					}
-					newFaces.Add(newFace);
-					p.faces = newFaces;
-				}
-				
-			}
-
-			newCube.appliedRotations.Add(Rotation.Rr);
-			return newCube;
-		}
-
-
-
-
-
-
-
 
 	}
+
+
+	abstract class RotationSettings {
+		public abstract PropertyInfo horizontalCoordinate { get; }
+		public abstract PropertyInfo verticalCoordinate { get; }
+		public abstract Dictionary<Direction, Direction> nextDirection { get; }
+		public abstract bool reversed { get; }
+	}
+
+
+
+	class FRotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("x");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("y");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Up, Direction.Right},
+			{Direction.Right, Direction.Down},
+			{Direction.Down, Direction.Left},
+			{Direction.Left, Direction.Up},
+			{Direction.Forwards, Direction.Forwards}
+		};
+		public override bool reversed => false;
+	}
+
+	class FrRotationSettings : FRotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => true;
+	}
+
+
+
+	class RRotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("z");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("y");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Up, Direction.Backwards},
+			{Direction.Backwards, Direction.Down},
+			{Direction.Down, Direction.Forwards},
+			{Direction.Forwards, Direction.Up},
+			{Direction.Right, Direction.Right}
+		};
+		public override bool reversed => false;
+	}
+
+	class RrRotationSettings : RRotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => true;
+	}
+
+
+
+	class URotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("x");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("z");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Forwards, Direction.Left},
+			{Direction.Left, Direction.Backwards},
+			{Direction.Backwards, Direction.Right},
+			{Direction.Right, Direction.Forwards},
+			{Direction.Up, Direction.Up}
+		};
+		public override bool reversed => false;
+	}
+
+	class UrRotationSettings : URotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => true;
+	}
+
+
+
+	class LRotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("z");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("y");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Up, Direction.Forwards},
+			{Direction.Forwards, Direction.Down},
+			{Direction.Down, Direction.Backwards},
+			{Direction.Backwards, Direction.Up},
+			{Direction.Left, Direction.Left}
+		};
+		public override bool reversed => true;
+	}
+
+	class LrRotationSettings : LRotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => false;
+	}
+
+
+
+	class BRotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("x");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("y");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Up, Direction.Left},
+			{Direction.Left, Direction.Down},
+			{Direction.Down, Direction.Right},
+			{Direction.Right, Direction.Up},
+			{Direction.Backwards, Direction.Backwards}
+		};
+		public override bool reversed => true;
+	}
+
+	class BrRotationSettings : BRotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => false;
+	}
+
+
+
+	class DRotationSettings : RotationSettings {
+		public override PropertyInfo horizontalCoordinate => typeof(Piece).GetProperty("x");
+		public override PropertyInfo verticalCoordinate => typeof(Piece).GetProperty("z");
+		public override Dictionary<Direction, Direction> nextDirection => new Dictionary<Direction, Direction>() {
+			{Direction.Forwards, Direction.Right},
+			{Direction.Right, Direction.Backwards},
+			{Direction.Backwards, Direction.Left},
+			{Direction.Left, Direction.Forwards},
+			{Direction.Down, Direction.Down}
+		};
+		public override bool reversed => true;
+	}
+
+	class DrRotationSettings : DRotationSettings {
+		public override Dictionary<Direction, Direction> nextDirection => base.nextDirection.ToDictionary(x => x.Value, x => x.Key);
+		public override bool reversed => false;
+	}
+
 }
--- RubiksCubeSolver/Program.cs
+++ RubiksCubeSolver/Program.cs
@@ -10,12 +10,12 @@ namespace RubiksCubeSolver {
 			cube.draw();
 			Console.WriteLine();
 			
-			cube = cube.applyRotation(Rotation.R);
-
-			cube.draw();
-			Console.WriteLine();
-
+			cube = cube.applyRotation(Rotation.Fr);
+			cube = cube.applyRotation(Rotation.L);
+			cube = cube.applyRotation(Rotation.U);
 			cube = cube.applyRotation(Rotation.Rr);
+			cube = cube.applyRotation(Rotation.Br);
+			cube = cube.applyRotation(Rotation.Lr);
 
 			cube.draw();
 			Console.WriteLine();
